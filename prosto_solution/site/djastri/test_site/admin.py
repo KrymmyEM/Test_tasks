0ff83deaeda3566d4afdf93301dcf29a345655bb
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2023-11-30 21:39:37 +0300

    [UP] Register models

--- prosto_solution/site/djastri/test_site/admin.py
+++ prosto_solution/site/djastri/test_site/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from test_site.models import Item, Order, OrderItem, Tax, Discount
+
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Tax)
+admin.site.register(Discount)
+
 # Register your models here.
