5f59f37940397106fdbf1a56e3f8488a34cfe6ed
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2023-11-30 12:12:07 +0300

    [UP] add views and paths

--- prosto_solution/site/djastri/test_site/urls.py
+++ prosto_solution/site/djastri/test_site/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from test_site import views
 
 urlpattern_site = [
-
+    path("buy/<int:id>/", views.BuyView.as_view()),
+    path("item/<int:id>/", views.ItemView.as_view()),
 ]
--- prosto_solution/site/djastri/test_site/views.py
+++ prosto_solution/site/djastri/test_site/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from django.http import JsonResponse, HttpResponse
 
 
-# Create your views here.
+class BuyView(View):
+    def get(self, request, id: int) -> JsonResponse:
+        ...
+
+class ItemView(TemplateView):
+    template_name = "item.html"
+    def get_context_data(self, id: int, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = id
+        return context
