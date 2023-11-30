89502abd46a1a7b441aefeffced8b46730cbedd8
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2023-11-30 21:00:52 +0300

    [UP] Add makeorder

--- prosto_solution/site/djastri/test_site/urls.py
+++ prosto_solution/site/djastri/test_site/urls.py
@@ -3,6 +3,7 @@ from test_site import views
 
 urlpattern_site = [
     path("buy/<int:id>/", views.ItemOrderView.as_view()),
+    path("makeorder/", views.MakeOrderView.as_view()),
     path("add/<int:id>/", views.AddItemView.as_view()),
     path("item/<int:id>/", views.ItemView.as_view()),
 ]
